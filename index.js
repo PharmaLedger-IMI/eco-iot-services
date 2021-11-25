7b102fa73e15c53e21da909864c0282fc90e09d2
mastaleru <mastaleru.rafael@gmail.com>
2021-11-25 18:59:52 +0200

    moved code

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    CommunicationService: require("./services/CommunicationService"),
+    DateTimeService: require("./services/DateTimeService"),
+    DIDService : require ("./services/DIDService"),
+    DSUService : require ("./services/DSUService"),
+    SharedStorage: require ("./services/SharedStorage"),
+    BaseRepository: require ("./repositories/BaseRepository"),
+    Constants : require ("./utils/Constants"),
+    FileDownloader : require ("./utils/FileDownloader"),
+    FileDownloaderService : require ("./utils/FileDownloaderService"),
+}

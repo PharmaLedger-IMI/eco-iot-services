8acd227ff97291317fb006b7b8eb406ff1cf85d8
paiusCatalin <p.cata95@gmail.com>
2021-12-03 11:01:22 +0200

    [Refactor] Services migration from clinical site to common-services

---
+++ models/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ConsentModel: require("./HL7/ConsentModel").getConsentModel(),
+    ContractModel: require("./HL7/ContractModel").getContractModel(),
+    GroupModel: require("./HL7/GroupModel").getGroupModel(),
+    OrganizationModel: require("./HL7/OrganizationModel").getOrganizationModel(),
+    PatientModel: require("./HL7/PatientModel").getPatientModel(),
+    PlanDefinitionModel: require("./HL7/PlanDefinitionModel").getPlanDefinitionModel(),
+    PractitionerModel: require("./HL7/PractitionerModel").getPractitionerModel(),
+    PractitionerRoleModel: require("./HL7/PractitionerRoleModel").getPractitionerRoleModel(),
+    QuestionnaireModel: require("./HL7/QuestionnaireModel").getQuestionnaireModel(),
+    QuestionnaireResponseModel: require("./HL7/QuestionnaireResponseModel").getQuestionnaireResponseModel(),
+    RelatedPersonModel: require("./HL7/RelatedPersonModel").getRelatedPersonModel(),
+    ResearchStudyModel: require("./HL7/ResearchStudyModel").getResearchStudyModel(),
+    ResearchSubjectModel: require("./HL7/ResearchSubjectModel").getResearchSubjectModel()
+};

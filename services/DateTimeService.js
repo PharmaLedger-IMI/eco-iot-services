4d485d84e7f22f70f391d05fb20f98d5e6f26749
mastaleru <mastaleru.rafael@gmail.com>
2022-01-31 14:35:52 +0200

    convert date to input[type="date"] value

--- services/DateTimeService.js
+++ services/DateTimeService.js
@@ -85,8 +85,23 @@ const timeAgo  = (time, shortType = false) => {
     return time;
 }
 
+function convertDateToInputValue(date) {
+    //<input type="date">the browsers support only value in the format yyyy-mm-dd
+    let month = '' + (date.getMonth() + 1);
+    let day = '' + date.getDate();
+    let year = date.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
 
 module.exports = {
+    convertDateToInputValue,
     convertStringToLocaleDate,
     convertStringToLocaleDateTimeString,
     getCurrentDate,

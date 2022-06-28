f59c648432bb93f97a01a8de75959c10357cfd92
nliappas <67898132+nliappas@users.noreply.github.com>
2022-06-28 17:32:08 +0200

    extra healthdata functions

--- services/iot/HealthDataService.js
+++ services/iot/HealthDataService.js
@@ -10,7 +10,11 @@ class HealthDataService extends DSUService {
 
     getAllObservations = (callback) => this.getEntities(callback);
 
+    getObservation = (uid, callback) => this.getEntity(uid, callback);
+
     saveObservation = (data, callback) => this.saveEntity(data, callback);
+
+    getSReadSSI = (pathPrefix, callback) => this.getEntityMountSSI(pathPrefix, callback);
 
 }
 

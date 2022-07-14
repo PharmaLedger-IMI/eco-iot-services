e3a65924a0237988d614020e6c204c626e47d7a7
paiuscatalin <p.cata95@gmail.com>
2022-07-14 08:26:59 +0300

    Integration of anonymised DID with VC #216

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     DSUService: require("./services/DSUService"),
     SharedStorage: require("./services/SharedStorage"),
     AbstractAPI: require("./services/AbstractAPI"),
+    JWTService: require("./services/JWTService"),
     BaseRepository: require("./repositories/BaseRepository"),
     Constants: require("./utils/Constants"),
     momentService: require('./utils/moment.min'),

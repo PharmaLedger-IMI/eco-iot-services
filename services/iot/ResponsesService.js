09e3b135014b51c7d88c8ba5b4ada54540365254
Cosmin Tuca <tuc@rms.ro>
2022-08-31 11:22:01 +0300

    update responses for questions already responded https://github.com/PharmaLedger-IMI/eco-iot-pmed-workspace/issues/418

--- services/iot/ResponsesService.js
+++ services/iot/ResponsesService.js
@@ -8,7 +8,29 @@ class ResponsesService extends DSUService {
 
     mount = (keySSI, callback) => this.mountEntity(keySSI, callback)
 
-    saveResponse = (response, callback) => this.saveEntity(response, callback)
+    saveResponse(response,callback) {
+        if (response.uid) {
+            return this.updateEntity(response, (err, data) => {
+                if (err) {
+                    return callback(err);
+                }
+                this.getMountedSSI(data.uid, (err, test) => {
+                    if (err) {
+                        return callback(err);
+                    }
+                    const profilesReadSSI = this.getSReadSSI(test);
+                    callback(undefined, profilesReadSSI);
+                })
+
+            });
+        }
+        this.saveEntity(response, (err, data) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(undefined, data.sReadSSI);
+        });
+    }
 
     getResponses = (callback) => this.getEntities(callback);
 

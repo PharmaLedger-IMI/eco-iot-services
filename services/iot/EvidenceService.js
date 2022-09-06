d5af161894c702e23c4403799b22bc70c3567e83
nliappas <67898132+nliappas@users.noreply.github.com>
2022-09-06 11:00:20 +0200

    upload evidence file

--- services/iot/EvidenceService.js
+++ services/iot/EvidenceService.js
@@ -16,6 +16,36 @@ class EvidenceService extends DSUService {
 
     updateEvidence = (data, callback) => this.updateEntity(data, callback);
 
+    addEvidenceFile(file, evidenceUid) {
+        return this.uploadFile("evidence" + '/' + evidenceUid + '/' + 'files' + '/' + file.name, file.file);
+    }
+
+    uploadFile(path, file) {
+        function getFileContentAsBuffer(file, callback) {
+            let fileReader = new FileReader();
+            fileReader.onload = function (evt) {
+                let arrayBuffer = fileReader.result;
+                callback(undefined, arrayBuffer);
+            };
+
+            fileReader.readAsArrayBuffer(file);
+        }
+
+        return new Promise((resolve, reject) => {
+            getFileContentAsBuffer(file, (err, arrayBuffer) => {
+                if (err) {
+                    return reject('Could not get file as a Buffer');
+                }
+                this.DSUStorage.writeFile(path, $$.Buffer.from(arrayBuffer), undefined, (err, keySSI) => {
+                    if (err) {
+                        return reject(new Error(err));
+                    }
+                    resolve();
+                });
+            });
+        });
+    }
+
 }
 
 module.exports = EvidenceService;
